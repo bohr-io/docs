231b836e7140f647b72526a12863f69c3e91485d
Willerson Stockler <willersonsp@hotmail.com>
2022-11-10 08:19:02 -0300

    fix edit page links.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,15 +34,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bohr-io/docs',
+            'https://github.com/bohr-io/docs/edit/main',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/bohr-io/docs',
         },
